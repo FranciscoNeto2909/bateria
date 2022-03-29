b5a3ae3217b00b9712d68cf7ff874e6ea5ae0491
Francisco Neto <wantedshark@gmail.com>
2022-03-29 10:36:07 -0300

    project base created

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-const elem1 = document.querySelector(".elem-1")
+const elem1 = document.querySelector(".elem")
+const elem2 = document.getElementById("elem-2")
+const elem3 = document.getElementById("elem-3")
+const elem4 = document.getElementById("elem-4")
+const elem5 = document.getElementById("elem-5")
+const elem6 = document.getElementById("elem-6")
+const elem7 = document.getElementById("elem-7")
+const elem8 = document.getElementById("elem-8")
+const elem9 = document.getElementById("elem-9")
 
 elem1.addEventListener("click",()=>{
-    
+    document.querySelector("#a1").play()
+})
+
+elem2.addEventListener("click",()=>{
+    document.querySelector("#a2").play()
+})
+
+elem3.addEventListener("click",()=>{
+    document.querySelector("#a3").play()
+})
+
+elem4.addEventListener("click",()=>{
+    document.querySelector("#a4").play()
+})
+
+elem5.addEventListener("click",()=>{
+    document.querySelector("#a5").play()
+})
+
+elem6.addEventListener("click",()=>{
+    document.querySelector("#a6").play()
+})
+
+elem7.addEventListener("click",()=>{
+    document.querySelector("#a7").play()
+})
+
+elem8.addEventListener("click",()=>{
+    document.querySelector("#a8").play()
+})
+
+elem9.addEventListener("click",()=>{
+    document.querySelector("#a9").play()
 })
